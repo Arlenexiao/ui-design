011b987618127796280d7505e1575fb42f6faecf
Jerry <jerryzhong@ideaculture.com>
2015-04-13 10:47:08 +0800

    *添加字体自适应效果

--- demo-slider/js/slider.js
+++ demo-slider/js/slider.js
@@ -46,8 +46,21 @@
       pageWidth=$(window).width();
       arrLeft=[0,-pageWidth,-pageWidth*2,-pageWidth*3,-pageWidth*4];
       $('#ulPage').css('left',arrLeft[pageNum]+'px');
+      setfontSize();
+      
   }
+
   function circle(){      
       $(".circle li").removeClass("circle-cur");
       $(".circle li").eq(pageNum-1).addClass("circle-cur");
+  }
+  //setfontSize();
+  function setfontSize(){
+      $(".responsive").css("font-size", Math.floor(50*pageWidth/1920));
+      $(".responsive2").css("width", Math.floor(200*pageWidth/1920));
+      $(".responsive2").css("height", Math.floor(80*pageWidth/1920));
+      $(".responsive2").css("line-height", Math.floor(80*pageWidth/1920)+'px');
+      $(".responsive2").css("font-size", Math.floor(25*pageWidth/1920));
+      $(".responsive3").css("font-size", Math.floor(20*pageWidth/1920));
+      $(".responsive3").css("width", Math.floor(600*pageWidth/1920));
   }
