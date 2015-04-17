a676fae6bfc06bac8ab200cb65fb0517d72abb92
Jerry <jerryzhong@ideaculture.com>
2015-04-17 13:59:04 +0800

    *change

--- scripts/nav.js
+++ scripts/nav.js
@@ -6,18 +6,18 @@ var pageWidth;
 
 $(document).ready(function(){
    pageWidth=$(window).width();
-   if(pageWidth<=750){
-   	$(".mainlevel").unbind("mousemove",slideDownHandler);
-   	$(".mainlevel").unbind("mouseleave",slideUpHandler);
-   	$(".mainlevel").bind("click",clickHandler);
-  }else{
-   	$(".mainlevel").bind("mousemove",slideDownHandler);
-   	$(".mainlevel").bind("mouseleave",slideUpHandler);
-   	$(".mainlevel").unbind("click",clickHandler);
-  }
+   $(".mainlevel").bind("mousemove",slideDownHandler);
+   $(".mainlevel").bind("mouseleave",slideUpHandler);
+   $(".mainlevel").bind("click",clickHandler);
+
+   if(pageWidth<=750)
+   	$(".mainlevel").find('ul').removeClass("position_absolute");
+   else
+   	$(".mainlevel").find('ul').addClass("position_absolute");
   
 });
 function clickHandler(event){
+	if(pageWidth>750) return; 
 	if($(this).find('ul').css("display")=="none")
 	{
 	    $(this).find('a').addClass("arrowbg");
@@ -28,22 +28,21 @@ function clickHandler(event){
 	}
 }
 function slideDownHandler(event){
+	  if(pageWidth<=750) return; 
 	  $(this).find('a').addClass("arrowbg");
 	  $(this).find('ul').slideDown("1000");
 }
 function slideUpHandler(event){
+	  if(pageWidth<=750) return; 
 	  $(this).find('a').removeClass("arrowbg");
 	  $(this).find('ul').slideUp("fast");
 }
 window.onresize = function(){
-    pageWidth=$(window).width(); 
-   if(pageWidth<=750){
-   	$(".mainlevel").unbind("mousemove",slideDownHandler);
-   	$(".mainlevel").unbind("mouseleave",slideUpHandler);
-   	$(".mainlevel").bind("click",clickHandler);
-  }else{
-   	$(".mainlevel").bind("mousemove",slideDownHandler);
-   	$(".mainlevel").bind("mouseleave",slideUpHandler);
-   	$(".mainlevel").unbind("click",clickHandler);
-  }
+    pageWidth=$(window).width();
+    $(".mainlevel").find('ul').css("display","none");
+    $(".mainlevel").find('a').removeClass("arrowbg");
+   if(pageWidth<=750)
+   	$(".mainlevel").find('ul').removeClass("position_absolute");
+   else
+   	$(".mainlevel").find('ul').addClass("position_absolute");
 }
