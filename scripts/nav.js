55b2c6aad11aaff6e4e8a9313db0f3d3e79e8c71
Jerry <jerryzhong@ideaculture.com>
2015-04-17 11:21:59 +0800

    *add nav  js

--- scripts/nav.js
+++ scripts/nav.js
@@ -2,3 +2,48 @@
 //     //alert($('.ServiseMenu').html())
 //     $('.ServiseMenu').removeClass('hide');
 // })
+var pageWidth;
+
+$(document).ready(function(){
+   pageWidth=$(window).width();
+   if(pageWidth<=750){
+   	$(".mainlevel").unbind("mousemove",slideDownHandler);
+   	$(".mainlevel").unbind("mouseleave",slideUpHandler);
+   	$(".mainlevel").bind("click",clickHandler);
+  }else{
+   	$(".mainlevel").bind("mousemove",slideDownHandler);
+   	$(".mainlevel").bind("mouseleave",slideUpHandler);
+   	$(".mainlevel").unbind("click",clickHandler);
+  }
+  
+});
+function clickHandler(event){
+	if($(this).find('ul').css("display")=="none")
+	{
+	    $(this).find('a').addClass("arrowbg");
+		$(this).find('ul').slideDown("1000");
+	}else{
+	    $(this).find('a').removeClass("arrowbg");
+	    $(this).find('ul').slideUp("fast");
+	}
+}
+function slideDownHandler(event){
+	  $(this).find('a').addClass("arrowbg");
+	  $(this).find('ul').slideDown("1000");
+}
+function slideUpHandler(event){
+	  $(this).find('a').removeClass("arrowbg");
+	  $(this).find('ul').slideUp("fast");
+}
+window.onresize = function(){
+    pageWidth=$(window).width(); 
+   if(pageWidth<=750){
+   	$(".mainlevel").unbind("mousemove",slideDownHandler);
+   	$(".mainlevel").unbind("mouseleave",slideUpHandler);
+   	$(".mainlevel").bind("click",clickHandler);
+  }else{
+   	$(".mainlevel").bind("mousemove",slideDownHandler);
+   	$(".mainlevel").bind("mouseleave",slideUpHandler);
+   	$(".mainlevel").unbind("click",clickHandler);
+  }
+}
